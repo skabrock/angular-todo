9d50a6018bf1a4741f931518b1190edf46a6ce22
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 18:53:51 +0300

    todo checking

--- main.js
+++ main.js
@@ -14,16 +14,32 @@ app.directive('toDo', function() {
     template: `
       <div ng-repeat='todo in todos'>
         <label>
-          <input type="checkbox" >
+          <input type="checkbox" ng-model="smth" ng-change="toggleTodo(todo.id)">
           {{todo.title}}
         </label>
       </div>
       <hr>
-      <input type="text">
-      <button>+ Add</button>
+      <input type="text" ng-model="todoText">
+      <button ng-click="addTodo()">+ Add</button>
     `,
     link: function(scope, element, attrs) {
       scope.todos = toDoList;
+      scope.addTodo = function() {
+        console.log('add task', );
+        var str = scope.todoText,
+            list = scope.todos;
+        if (str.length) {
+          list.push({
+            id: list.length ? list[list.length - 1].id + 1 : 0,
+            title: str,
+            checked: false
+          })
+          str = '';
+        }
+      }
+      scope.toggleTodo = function(id) {
+        console.log(id);
+      }
     }
   }
 })
