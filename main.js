f06887c3b2677a10affb23014c3e7cc5e1a89f9d
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 18:25:43 +0300

    UI ready

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 var app = angular.module('app', []);
+
+app.directive('toDo', function() {
+  var toDoList = [
+    {
+      id: 0,
+      title: 'Make a todo app on angularjs',
+      checked: false
+    }
+  ];
+
+  return {
+    restrict: 'E',
+    template: `
+      <div ng-repeat='todo in todos'>
+        <label>
+          <input type="checkbox" >
+          {{todo.title}}
+        </label>
+      </div>
+      <hr>
+      <input type="text">
+      <button>+ Add</button>
+    `,
+    link: function(scope, element, attrs) {
+      scope.todos = toDoList;
+    }
+  }
+})
