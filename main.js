bd4152a2473896002018d7a6733acd23e7e450b4
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-07 15:40:40 +0300

    edit, remove, save on enter, empty list

--- main.js
+++ main.js
@@ -5,25 +5,39 @@ app.directive('toDo', function() {
     {
       id: 0,
       title: 'Make a todo app on angularjs',
+      checked: true
+    }, {
+      id: 1,
+      title: 'Send it to Taya',
+      checked: true
+    }, {
+      id: 2,
+      title: 'Taya likes it?',
       checked: false
     }
   ];
+  var editingTodo = null;
 
   return {
     restrict: 'E',
     template: `
       <div ng-repeat='(i, todo) in todos'>
-        <label ng-style="{textDecoration: todo.checked ? 'line-through' : 'none'}">
-          <input type="checkbox" ng-model="todos[i].checked">
-          {{todo.title}}
+        <label>
+          <input type="checkbox" ng-model="todo.checked">
+          <span class="todo" ng-hide="editingTodo === todo.id" ng-style="{textDecoration: todo.checked ? 'line-through' : 'none'}">{{todo.title}}</span>
+          <input class="todo" ng-show="editingTodo === todo.id" type="text" ng-model="todos[i].title" ng-blur="editTodo()" ng-keyup="$event.keyCode == 13 && editTodo()" />
         </label>
+        <button title="edit" class="button {{editingTodo === todo.id ? 'fa-check' : 'fa-edit'}}" ng-click="editTodo(i)"></button>
+        <button title="remove" class="button fa-close" ng-click="removeTodo(i)"></button>
       </div>
+      <div ng-show="!todos.length">Empty todo list</div>
       <hr>
-      <input type="text" ng-model="todoText">
-      <button ng-click="addTodo()">+ Add</button>
+      <input type="text" ng-model="todoText" ng-keyup="$event.keyCode == 13 && addTodo()">
+      <button title="add" class="button fa-plus" ng-click="addTodo()"> Add</button>
     `,
     link: function(scope, element, attrs) {
       scope.todos = toDoList;
+      scope.editingTodo = editingTodo;
       scope.addTodo = function() {
         var str = scope.todoText,
             list = scope.todos;
@@ -35,6 +49,12 @@ app.directive('toDo', function() {
           })
           scope.todoText = '';
         }
+      }
+      scope.removeTodo = function(i) {
+        scope.todos.splice(i, 1);
+      }
+      scope.editTodo = function(i) {
+        scope.editingTodo =  scope.editingTodo !== i ? i : null;
       }
     }
   }
